9de811ceb8b8b834d0048190751039a64f10b64d
awsdevopskiran <111186825+awsdevopskiran@users.noreply.github.com>
2022-12-24 22:52:03 +0530

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -2,7 +2,7 @@
 
 resource "aws_vpc" "terraform_Aws_VPC" {
 #   cidr_block           = var.vpc_cidr
-  cidr_block           = 171.31.0.0/16
+  cidr_block           = 12.0.0.16/16
   enable_dns_hostnames = true
   enable_dns_support   = true
 
